98ff8e9c5fba6df1cdc6770a9dee4bf1483edd23
Sitharam1 <venkatmadasu741@gmail.com>
2024-05-20 17:36:53 +0530

    feature202

---
+++ Application.java
@@ -0,0 +1,42 @@
+// File: MyClass.java
+
+// A class with public access specifier
+public class MyClass {
+    
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+    #feature202 changes by Ramesh on Oct.2023
+     public void display2(int a) {
+    System.out.println("Method with a single parameter: " + a);
+  }
+
+  public static void main(String[] args) {
+    
+    // create an object of Main
+    Main obj = new Main();
+
+    // calling method with no parameter
+    obj.display1();
+    
+    // calling method with the single parameter
+    obj.display2(24);
+  }
+}
+}
